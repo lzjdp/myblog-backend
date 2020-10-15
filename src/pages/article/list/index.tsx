246cbb0b1f8050389a52806acae1d89e9bb35940
carr <carr@163.com>
2020-10-15 20:02:14 +0800

    2.1.1

--- src/pages/article/list/index.tsx
+++ src/pages/article/list/index.tsx
@@ -7,51 +7,37 @@ const ArticleList:FC = () => {
   const [selectedRowsState, setSelectedRowsState] = useState([]);
   const [deleteModal, setDeleteModal] = useState(false);
   const ref = useRef<ActionType>();
-  // const columns = [
-  //   {
-  //     title: '序号',
-  //     dataIndex: 'index',
-  //     valueType: 'indexBorder',
-  //     width: 72
-  //   },
-  //   {
-  //     title: '编号',
-  //     dataIndex: 'article_id',
-  //   },
-  //   {
-  //     title: '标题',
-  //     dataIndex: 'article_title',
-  //     valueType: 'text'
-  //   },
-  //   {
-  //     title: '分类',
-  //     dataIndex: 'article_classify',
-  //     valueType: 'text'
-  //   },
-  //   {
-  //     title: '创建时间',
-  //     dataIndex: 'article_createTime',
-  //     valueType: 'dataTime'
-  //   }
-  // ];
   const columns = [
     {
+      title: '序号',
+      dataIndex: 'index',
+      valueType: 'indexBorder',
+      width: 72
+    },
+    {
+      title: '编号',
+      dataIndex: 'article_id',
+    },
+    {
       title: '标题',
-      dataIndex: 'title'
+      dataIndex: 'article_title',
+      valueType: 'text'
     },
     {
-      title: '作者',
-      dataIndex: 'author'
+      title: '分类',
+      dataIndex: 'article_classify',
+      valueType: 'text'
     },
     {
-      title: '阅读量',
-      dataIndex: 'views'
+      title: '创建时间',
+      dataIndex: 'article_createTime',
+      valueType: 'dataTime'
     }
-  ]
-  const handleRemove = async (selectedRows) => {
+  ];
+  const handleRemove = (selectedRows) => {
     const hide = message.loading('正在删除');
     if (!selectedRows) return true;
-    await removeArticles({
+    removeArticles({
       id: selectedRows.map(row => row.article_id)
     }).then(res => {
       hide();
@@ -74,38 +60,20 @@ const ArticleList:FC = () => {
     <PageContainer>
       <ProTable
         columns={columns}
-        // rowSelection={{
-        //   onChange: (_, selecredRows) => setSelectedRowsState(selecredRows)
-        // }}
-        // request={(params) => {
-        //   return new Promise((resolve, reject) => {
-        //     getArticleList(params).then(res => {
-        //       const data = res.data
-        //       resolve(data)
-        //     }).catch(error => {
-        //       message.error(error)
-        //     })
-        //   })
-        // }}
-        // actionRef={ref}
-        request={async () => {
-          return {
-            data: [
-              {
-                key: 1,
-                title: 'umi+egg搭建个人博客(一)',
-                author: 'carr',
-                views: 2000
-              },
-              {
-                key: 2,
-                title: 'umi+egg搭建个人博客(二)',
-                author: 'carr',
-                views: 2800
-              }
-            ]
-          }
+        rowSelection={{
+          onChange: (_, selectedRows) => setSelectedRowsState(selectedRows)
         }}
+        request={(params) => {
+          return new Promise((resolve, reject) => {
+            getArticleList(params).then(res => {
+              const data = res.data
+              resolve(data)
+            }).catch(error => {
+              message.error(error)
+            })
+          })
+        }}
+        actionRef={ref}
       />
       {
         selectedRowsState.length > 0 && (<FooterToolbar
